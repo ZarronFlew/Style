9dbec14bb2deb1482df9f591b80f39a2d89bb182
ZarronFlew <103321009+ZarronFlew@users.noreply.github.com>
2023-01-13 17:30:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let start = document.querySelector('.main-btn');
+start.addEventListener('click', () => {
+    e.preventDefault();
+    // console.log("hello")ж
+    // const scrollTarget = document.querySelector(".filter");
+    // const topOffset = document.querySelector('.scrollto').offsetHeight;
+    // const topOffset = 0; // если не нужен отступ сверху 
+    // const elementPosition = scrollTarget.getBoundingClientRect().top;
+    // const offsetPosition = elementPosition - topOffset;
+    // window.scrollBy({
+    //     top: offsetPosition,
+    //     behavior: 'smooth'
+    // });
+})
